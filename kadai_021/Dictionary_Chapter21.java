3445ccaa96155af3b7ba1d8c28f2ec19fa762fc9
obi1105 <obi1105@e-corporation.co.jp>
2026-05-27 14:19:24 +0900

    kadai_021をプッシュ

---
+++ kadai_021/Dictionary_Chapter21.java
@@ -0,0 +1,37 @@
+package kadai_021;
+
+import java.util.HashMap;
+
+public class Dictionary_Chapter21 {
+		
+		private HashMap <String,String> dictionary = new HashMap <String,String>();
+		
+		public void Change(String[] words){
+		dictionary.put("apple", "りんご");
+		dictionary.put("peach", "桃");
+		dictionary.put("banana", "バナナ");
+		dictionary.put("lemon", "レモン");
+		dictionary.put("pear", "梨");
+		dictionary.put("kiwi", "キウィ");
+		dictionary.put("strawberry", "いちご");
+		dictionary.put("grape", "ぶどう");
+		dictionary.put("muscut", "マスカット");
+		dictionary.put("cherry", "さくらんぼ");
+		
+		for(String check : words) {
+			String val = dictionary.get(check);
+			
+			if(dictionary.get(check) != null ) {
+				System.out.println( check + "の意味は"+ val);
+			} else {
+				System.out.println(check + "は辞書に存在しません");
+			}
+
+		}
+
+	}
+}
+
+	
+	
+
