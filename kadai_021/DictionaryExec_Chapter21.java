3987debee7b87ca1ed0d39f32e05b07fcbb38470
obi1105 <obi1105@e-corporation.co.jp>
2026-05-27 14:01:21 +0900

    kadai_021をプッシュ

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,12 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+	public static void main(String[] args) {
+	
+	String[] words = {"apple","banana","grape","orange"};
+	Dictionary_Chapter21 dictionary2 = new Dictionary_Chapter21(words);
+	
+	
+	
+	}
+}
